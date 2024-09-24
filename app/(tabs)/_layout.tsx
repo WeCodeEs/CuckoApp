001623f278369204fd93617a218e021a038e7aa2
Carlos Javier Gonzalez Bautista <carlos.gonzalezba@anahuac.mx>
2024-09-23 21:02:27 -0600

    Creación de la tab carrito y del archivo cart.tsx al cual redirige la tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+// import { CustomHeader } from '@/components/CustomHeader';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -11,24 +12,43 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        //Indica el color de una tab cuando está activa o no
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tabIconSelected,
-        headerShown: false,
+        //Oculta el "title" de cada tabIcon
+        tabBarShowLabel: false,
+        //Muestra u oculta el header
+        // headerShown: false,
+        //Muestra un header personalizado
+        // header: () => <CustomHeader />,
       }}>
       <Tabs.Screen
+        //Define a qué vista se redirigirá cuando se de clic sobre la tab
         name="(home)"
         options={{
+          //Define el texto que se mostrará debajo del ícono
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
+            //Define el ícono que se mostrará
             <TabBarIcon name={focused ? 'House' : 'House'} color={color} />
+            //Los nombres de ícono compuestos no usan guiones, sino CammelCase
           ),
         }}
       />
       <Tabs.Screen
         name="favorites"
         options={{
-          title: 'Favs',
+          title: 'Favoritos',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'Heart' : 'Heart'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Carrito',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'ShoppingCart' : 'ShoppingCart'} color={color} />
           ),
         }}
       />
