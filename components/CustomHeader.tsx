ab85a94f545196d68dd686e246bfa4458ce21983
Carlos Javier Gonzalez Bautista <carlos.gonzalezba@anahuac.mx>
2024-10-04 12:38:48 -0600

    Incorporación de Grid para alineación de componentes

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -7,6 +7,7 @@ import { Divider } from '@/components/ui/divider';
 import { Avatar,AvatarBadge,AvatarFallbackText,AvatarImage } from '@/components/ui/avatar';
 import { Box } from '@/components/ui/box';
 import { HStack } from './ui/hstack';
+import { Grid, GridItem } from '@/components/ui/grid';
 
 export function CustomHeader() {
   const navigation = useNavigation();
@@ -14,7 +15,43 @@ export function CustomHeader() {
   return (
     <>
     <SafeAreaView style={styles.container}>
-        <HStack className="justify-between items-center px-2 py-2 bg-white w-full">
+
+    <Grid className="gap-2 px-2" _extra={{ className: 'grid-cols-8' }} >
+      <GridItem
+        className=""
+        _extra= {{ className: 'col-span-2' }}
+      />
+      <GridItem
+        className="items-center"
+        _extra={{ className: 'col-span-4' }}
+      >
+        <Box className="w-full items-center">
+          <Image 
+              className="rounded-full w-38 h-full"
+              source={require('@/assets/images/CuckoLogoTop.png')} 
+              alt="Logo Superior Cuckoo"
+              resizeMode="contain"
+          />
+        </Box>
+      </GridItem>
+      <GridItem
+        className="flex items-end justify-center"
+        _extra= {{ className: 'col-span-2' }}
+      >
+        <Box className="h-12 w-12">
+          <Avatar className="w-full h-full" >
+              <AvatarFallbackText>Carlos Gonzalez</AvatarFallbackText>
+              <AvatarImage
+              source={{
+                  uri: 'https://media.licdn.com/dms/image/v2/D5603AQEz4rRbs8-dEg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718265562308?e=1732752000&v=beta&t=xhngKflrHu1ehoSQuZMhgZ8rXAvBZFzH1B_wZmLiMu4',
+              }}
+              />
+              <AvatarBadge />
+          </Avatar>
+        </Box>
+      </GridItem>
+    </Grid>
+        {/* <HStack className="justify-between items-center px-2 py-2 bg-white w-full">
             <Box className="w-12 h-12"/>
             <Box className="w-3/4 flex items-center">
                 <Image 
@@ -36,7 +73,7 @@ export function CustomHeader() {
                     <AvatarBadge />
                 </Avatar>
             </Box>
-        </HStack>
+        </HStack> */}
       <Divider/>
     </SafeAreaView>
     </>
