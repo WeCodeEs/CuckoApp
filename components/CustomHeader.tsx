4b7a73de9f6f4201112fb4778f45b72112e9ac33
Carlos Javier Gonzalez Bautista <carlos.gonzalezba@anahuac.mx>
2024-10-04 12:43:22 -0600

    Eliminación de componentes innecesarios

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -51,30 +51,7 @@ export function CustomHeader() {
         </Box>
       </GridItem>
     </Grid>
-        {/* <HStack className="justify-between items-center px-2 py-2 bg-white w-full">
-            <Box className="w-12 h-12"/>
-            <Box className="w-3/4 flex items-center">
-                <Image 
-                    className="rounded-full w-2/3 h-full"
-                    source={require('@/assets/images/CuckoLogoTop.png')} 
-                    alt="Logo Superior Cuckoo"
-                    size='xl'
-                    resizeMode="contain"
-                />
-            </Box>
-            <Box className="h-12 w-12">
-                <Avatar className="w-full h-full" >
-                    <AvatarFallbackText>Carlos Gonzalez</AvatarFallbackText>
-                    <AvatarImage
-                    source={{
-                        uri: 'https://media.licdn.com/dms/image/v2/D5603AQEz4rRbs8-dEg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718265562308?e=1732752000&v=beta&t=xhngKflrHu1ehoSQuZMhgZ8rXAvBZFzH1B_wZmLiMu4',
-                    }}
-                    />
-                    <AvatarBadge />
-                </Avatar>
-            </Box>
-        </HStack> */}
-      <Divider/>
+    <Divider/>
     </SafeAreaView>
     </>
   );
