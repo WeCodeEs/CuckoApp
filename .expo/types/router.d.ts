cb7f5cfa172246fd4d09f3561141b8bbbcc9b79e
Jair Gomez Vasquez <jair.gomezv06@anahuac.mx>
2024-10-16 02:11:07 -0600

    WECK-16: Temp merge

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(home)` | `/(home)/` | `/(home)/details` | `/(tabs)` | `/(tabs)/` | `/(tabs)/(home)` | `/(tabs)/(home)/` | `/(tabs)/(home)/details` | `/(tabs)/cart` | `/(tabs)/details` | `/(tabs)/favorites` | `/(tabs)\(home)\` | `/(tabs)\(home)\_layout` | `/(tabs)\(home)\detail_product` | `/(tabs)\_layout` | `/(tabs)\favorites` | `/..\constants\platillos` | `/_sitemap` | `/cart` | `/details` | `/favorites`;
+      StaticRoutes: `/` | `/(home)` | `/(home)/` | `/(home)/detail_product` | `/(home)/details` | `/(tabs)` | `/(tabs)/` | `/(tabs)/(home)` | `/(tabs)/(home)/` | `/(tabs)/(home)/detail_product` | `/(tabs)/(home)/details` | `/(tabs)/detail_product` | `/(tabs)/details` | `/(tabs)/favorites` | `/_sitemap` | `/detail_product` | `/details` | `/favorites`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- screens/MenuScreen.jsx
+++ screens/MenuScreen.jsx
@@ -3,9 +3,19 @@ import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Image } from "@/components/ui/image";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 import { Center } from "@/components/ui/center";
 import { useNavigation } from '@react-navigation/native';
+import { useState } from "react";
+// import platillos from '../constants/platillos';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField, InputSlot } from "@/components/ui/input";
+import { Box } from "@/components/ui/box";
+import { Search, Coffee, Sandwich, ChefHat, Salad, ForkKnife, GlassWater} from "lucide-react-native";
+import { Icon } from '@/components/ui/icon';
+import { HStack } from '@/components/ui/hstack';
 
 const platillos = [
   { id: 1, nombre: 'Ensalada Cajún', imagen: require('@/assets/images/platillos/ensalada_cajun.png'), precio: '$55.00' },
@@ -16,26 +26,103 @@ const platillos = [
 
 const MenuScreen = () => {
   const navigation = useNavigation();
+  const [activeButton, setActiveButton] = useState("comida");
 
   return (
-    <Center className="bg-white h-full w-full" style={styles.container}>
-      <View className="flex flex-row flex-wrap justify-around" style={styles.grid}>
-        {platillos.map(platillo => (
-          <VStack key={platillo.id} className="items-center mb-4" style={styles.itemContainer}>
-            <Image source={platillo.imagen} alt="imagen" className="w-24 h-24 rounded-full" />
-            <Text size="lg" className="mt-2">{platillo.nombre}</Text>
-            <Text size="lg" className="mt-1 font-semibold text-orange-500">{platillo.precio}</Text>
-            <Button
-              size="xl"
-              className="mt-2 bg-blue-500 p-2 rounded-full"
-              onPress={() => navigation.navigate('detail_product', { platilloId: platillo.id })}
-            >
-              <ButtonText className="text-white text-xl">+</ButtonText>
-            </Button>
-          </VStack>
-        ))}
-      </View>
-    </Center>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Center style={styles.center}>
+          <Box style={styles.box}>
+            <VStack style={styles.vStack}>
+              <Box style={styles.headingBox}>
+                <Heading size={"lg"} bold="false" style={styles.headingLeft}>
+                  ¿Qué te gustaría{'\n'}comer hoy?
+                </Heading>
+              </Box>
+
+              <Input size="xl" style={styles.inputContainer}>
+                <InputSlot>
+                  <Icon as={Search} size="md" />
+                </InputSlot>
+                <InputField
+                    style={styles.inputFieldLarge}
+                    placeholder="Buscar"
+                />
+              </Input>
+
+              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <HStack space="sm" style={styles.buttonRow}>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("comida")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "comida" && styles.activeIconContainer]}>
+                        <Icon as={ChefHat} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Comida</Text>
+                  </VStack>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("snacks")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "snacks" && styles.activeIconContainer]}>
+                        <Icon as={Sandwich} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Snacks</Text>
+                  </VStack>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("ensaladas")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "ensaladas" && styles.activeIconContainer]}>
+                        <Icon as={Salad} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Ensaladas</Text>
+                  </VStack>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("platillos")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "platillos" && styles.activeIconContainer]}>
+                        <Icon as={ForkKnife} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Platillos</Text>
+                  </VStack>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("cafe")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "cafe" && styles.activeIconContainer]}>
+                        <Icon as={Coffee} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Café</Text>
+                  </VStack>
+                  <VStack alignItems="center">
+                    <Button onPress={() => setActiveButton("limonadas")} variant="link">
+                      <Box style={[styles.iconContainer, activeButton === "limonadas" && styles.activeIconContainer]}>
+                        <Icon as={GlassWater} size="20" />
+                      </Box>
+                    </Button>
+                    <Text style={styles.activeButtonText}>Limonadas</Text>
+                  </VStack>
+                </HStack>
+              </ScrollView>
+
+              <View style={styles.grid}>
+                {platillos.map(platillo => (
+                    <VStack key={platillo.id}  style={styles.vStackItem}>
+                      <Image size="xl" source={platillo.imagen} alt={platillo.nombre} style={styles.image} />
+                      <Text size="lg" bold="true" style={styles.itemText}>{platillo.nombre}</Text>
+                      <Text size="lg" bold="true" style={styles.itemPrice}>{platillo.precio}</Text>
+                      <Button
+                          size="sm"
+                          style={styles.addButton}
+                          onPress={() => navigation.navigate('detail_product', { platilloId: platillo.id })}
+                      >
+                        <ButtonText style={styles.addButtonText}>+</ButtonText>
+                      </Button>
+                    </VStack>
+                ))}
+              </View>
+            </VStack>
+          </Box>
+        </Center>
+      </ScrollView>
+
   );
 };
 
@@ -47,13 +134,129 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   grid: {
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingVertical: 1,
   },
   itemContainer: {
     width: '45%',
     alignItems: 'center',
+  },
+  scrollViewContent: {
+    padding: 0,
+  },
+  center: {
+    backgroundColor: 'white',
+    width: '100%',
+    flex: 1,
+  },
+  box: {
+    marginHorizontal: 5,
+    marginVertical: 5,
+    padding: 5,
+    maxWidth: '96%',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0,
+    elevation: 1,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  vStack: {
+    paddingBottom: 24,
+    flex: 1,
+  },
+  headingBox: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  headingLeft: {
+    textAlign: 'left',
+    marginLeft: 5,
+    marginBottom: 15,
+    fontWeight: 'normal',
+  },
+  inputContainer: {
+    width: '100%',
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingVertical: 2,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 0,
+  },
+  inputFieldLarge: {
+    flex: 1,
+    paddingLeft: 15,
+    fontSize: 18,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: -5,
+  },
+  iconContainer: {
+    marginHorizontal: 1,
+    borderWidth: 0,
+    borderColor: '#ccc',
+    borderRadius: 15,
+    padding: 15,
+    alignItems: 'center',
+    width: 50,
+    height: 60,
+  },
+  activeIconContainer: {
+    backgroundColor: '#f07122',
+    borderWidth: 0,
+  },
+  buttonText: {
+    color: '#888',
+    marginTop: 8,
+    fontSize: 9,
+  },
+  activeButtonText: {
+    color: 'black',
+    marginTop: 8,
+    fontSize: 10,
+  },
+  vStackItem: {
+    alignItems: 'center',
+    width: '50%',
+    marginBottom: 16,
+  },
+  image: {
+    width: 96,
+    height: 96,
+    borderRadius: 50,
+  },
+  itemText: {
+    marginTop: 8,
+    fontSize: 12,
+  },
+  itemPrice: {
+    marginTop: 3,
+    fontWeight: 'bold',
+    color: '#f07122',
+  },
+  addButton: {
+    marginTop: 6,
+    backgroundColor: '#49bcce',
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 0,
+  },
+  addButtonText: {
+    color: 'white',
+    fontSize: 24,
+    lineHeight: 30,
+    fontWeight: 'normal',
   },
 });
