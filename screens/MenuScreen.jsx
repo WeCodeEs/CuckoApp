359add3beb1e1ffc323a75846907a2d91f7088d0
Jair Gomez Vasquez <jair.gomezv06@anahuac.mx>
2024-11-01 21:38:20 -0600

    WECK-30: Fix Promociones Button

--- screens/MenuScreen.jsx
+++ screens/MenuScreen.jsx
@@ -282,6 +282,7 @@ const styles = StyleSheet.create({
   iconContainer: {
     marginHorizontal: 1,
     borderWidth: 0,
+    borderColor: '#ccc',
     borderRadius: 15,
     padding: 15,
     alignItems: 'center',
