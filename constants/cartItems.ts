1912de2fa0edebbc624848bcb07a65cd02585f1d
Carlos Gonzalez <carlos.gonzalezba@anahuac.mx>
2025-02-24 20:55:25 -0600

    WECK-80: Nuevas funciones para manejo de carrito
    -Se añadieron las funciones increaseCartItemQuantity, decreaseCartItemQuantity, y emptyCart para el manejo de items en el carrito.

--- constants/cartItems.ts
+++ constants/cartItems.ts
@@ -1,4 +1,4 @@
-import { Product, Variant, Ingredient, CartItem } from '@/constants/types';
+import { CartItem } from '@/constants/types';
 
 export let cartItems: CartItem[] = [];
 export let totalCartValue: number = 0;
@@ -40,6 +40,37 @@ export const removeCartItem = (cartItemToRemove: CartItem) => {
       JSON.stringify(cartItem.ingredients) === JSON.stringify(cartItemToRemove.ingredients)
     )
   );
+};
+
+export const increaseCartItemQuantity = (cartItemToIncrease: CartItem, addedQuantity: number) => {
+  console.log(`El item ${cartItemToIncrease} ha incrementado su cantidad en ${addedQuantity}`);
+  const selectedCartItem = cartItems.find(cartItem =>
+    JSON.stringify(cartItem.product) === JSON.stringify(cartItemToIncrease.product) &&
+    JSON.stringify(cartItem.selectedVariant) === JSON.stringify(cartItemToIncrease.selectedVariant) &&
+    JSON.stringify(cartItem.ingredients) === JSON.stringify(cartItemToIncrease.ingredients)
+  );
+  selectedCartItem!.quantity+= addedQuantity;
+  const changeInValue = addedQuantity * (selectedCartItem!.unitPrice);
+  updateTotalCartValue(changeInValue);
+};
+
+export const dereaseCartItemQuantity = (cartItemToDecrease: CartItem, removedQuantity: number) => {
+  console.log(`El item ${cartItemToDecrease} ha reducido su cantidad en ${removedQuantity}`);
+  const selectedCartItem = cartItems.find(cartItem =>
+    JSON.stringify(cartItem.product) === JSON.stringify(cartItemToDecrease.product) &&
+    JSON.stringify(cartItem.selectedVariant) === JSON.stringify(cartItemToDecrease.selectedVariant) &&
+    JSON.stringify(cartItem.ingredients) === JSON.stringify(cartItemToDecrease.ingredients)
+  );
+  selectedCartItem!.quantity-= removedQuantity;
+  const changeInValue = -removedQuantity * (selectedCartItem!.unitPrice);
+  updateTotalCartValue(changeInValue);
+};
+
+export const emptyCart = () => {
+  console.log(`Se ha vaciado el carrito`);
+  cartItems.length = 0;
+  const currentValue = getTotalCartValue();
+  updateTotalCartValue(-currentValue);
 };
 
 const updateTotalCartValue = (changeInValue: number) => {
