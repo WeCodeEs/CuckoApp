f15d32d3cd7f74b75c6743bdab027f31f348d5a1
Jair Gomez Vasquez <jair.gomezv06@anahuac.mx>
2025-03-05 19:32:56 -0600

    WECK-81: ADD Stripe integration for payments.

--- app/(tabs)/(cart)/_layout.tsx
+++ app/(tabs)/(cart)/_layout.tsx
@@ -1,15 +1,20 @@
 import { Stack } from 'expo-router';
 import { CustomHeader } from '@/components/CustomHeader';
+import { StripeProvider } from '@stripe/stripe-react-native';
+
+const STRIPE_PUBLIC_KEY = process.env.EXPO_PUBLIC_STRIPE_PUBLIC_KEY ?? "";
 
 export default function CartStack() {
   return (
-    <Stack
-      screenOptions={{
-        header: () => <CustomHeader />                
-      }}
-    >
-      <Stack.Screen name="cartScreen" options={{ title: 'Carrito', headerShown: false}} />
-      <Stack.Screen name="cartSchedule" options={{ title: 'Programar entrega', headerShown: false}} />
-    </Stack>
+    <StripeProvider publishableKey={STRIPE_PUBLIC_KEY}>
+      <Stack
+        screenOptions={{
+          header: () => <CustomHeader />                
+        }}
+      >
+        <Stack.Screen name="cartScreen" options={{ title: 'Carrito', headerShown: false}} />
+        <Stack.Screen name="cartSchedule" options={{ title: 'Programar entrega', headerShown: false}} />
+      </Stack>
+    </StripeProvider>
   );
 }
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,14 +2,14 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_REACT_NATIVE_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_REACT_NATIVE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
